2d3a3cd4eab906b6a0f551d4140c7e80abc6cf62
Satyam Lal <ssatyamlal@gmail.com>
2025-10-02 02:26:13 +0530

    Added glowing & sparkles effect for each menu item

--- src/components/ui/sparkles.tsx
+++ src/components/ui/sparkles.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useId, useMemo } from "react";
+import React, { useId } from "react";
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import type { Container, SingleOrMultiple } from "@tsparticles/engine";
@@ -84,7 +84,7 @@ export const SparklesCore = (props: ParticlesProps) => {
               },
               modes: {
                 push: {
-                  quantity: 4,
+                  quantity: 40,
                 },
                 repulse: {
                   distance: 200,
