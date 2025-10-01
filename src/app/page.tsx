70b5f1ff4b0e817ce3c6cf17818a0a1d8963588c
Satyam Lal <ssatyamlal@gmail.com>
2025-10-01 22:30:17 +0530

    started working on HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.2]">
-      <h1 className="text-5xl flex items-center justify-center">
+      <h1 className="min-h-screen text-5xl flex items-center justify-center">
         Music Course Platform
       </h1>
     </main>
